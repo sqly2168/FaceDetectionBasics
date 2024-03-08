90c898afc4e8a5f3a4d8c309aa92303899f79230
sqly2168 <133359160+sqly2168@users.noreply.github.com>
2024-03-08 17:59:09 +0100

    megértettem a kódot és kiegészítettem hogy az otthoni pc-ről is lehessen futtatni

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import cv2
 
-# Kaszkád fájl betöltése
-face_cascade = cv2.CascadeClassifier('C:\\Users\\SQLY\\AppData\\Local\\Packages\\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\\LocalCache\\local-packages\\Python312\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml')
+# Kaszkád fájl betöltése (arcfelismerés haar algoritmussal)
+#laptop
+#face_cascade = cv2.CascadeClassifier('C:\\Users\\SQLY\\AppData\\Local\\Packages\\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\\LocalCache\\local-packages\\Python312\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml')
+
+#otthoni pc
+face_cascade = cv2.CascadeClassifier('C:\\Users\\sqly2\\PycharmProjects\\FaceDetectionBasics\\venv\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml')
 
 #képbeolvasás
-kep = cv2.imread("C:\\Users\\SQLY\\Pictures\\Camera Roll\\Face.jpg")
+#laptop
+#kep = cv2.imread("C:\\Users\\SQLY\\Pictures\\Camera Roll\\Face.jpg")
+
+#otthoni pc
+kep = cv2.imread("C:\\Users\\sqly2\\Desktop\\kep.png")
 
 # Szürkeárnyalatos képpé alakítja (szükséges a kaszkád osztályozó számára)
 gray = cv2.cvtColor(kep, cv2.COLOR_BGR2GRAY)
