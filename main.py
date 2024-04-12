09038fb740cab45133b75836e557acca639fba5e
sqly2168 <133359160+sqly2168@users.noreply.github.com>
2024-04-12 14:24:09 +0200

    az arcok észlelése funkciót függvényesítettem, valamint készült egy euklidészi távolság fgv is

--- main.py
+++ main.py
@@ -1,5 +1,50 @@
 import cv2
 from sklearn.decomposition import PCA
+import math
+from jellemzok_kinyerve import adatbazis_jellemzok_lista
+
+def keresett_arc_jellemzok(kep):
+    """
+    Kivonja a jellemzőket a bemeneti képről.
+
+    Args:
+        kep: A feldolgozandó kép.
+
+    Returns:
+        list: A kép jellemzői.
+    """
+
+    try:
+        for (x, y, w, h) in faces_detect:
+            face_extract = gray[y:y + h, x:x + w]
+            arckep_meret = (100, 100)
+            arckep = cv2.resize(face_extract, arckep_meret)
+            arckep_vektor = arckep.flatten()
+            pca.fit(arckep_vektor.reshape(-1, 1))
+            transzformalt_arckep = pca.transform(arckep_vektor.reshape(-1, 1))
+            jellemzok = transzformalt_arckep.flatten()  # Jellemzők tárolása
+            return jellemzok
+        
+    except IndexError:
+        print("Nincs arc a képen.")
+
+    return None
+
+def euklideszi_tavolsag(vektor1, vektor2):
+    """
+    Euklidészi távolság kiszámítása két jellemzővektor között.
+
+    Args:
+        jellemzok1 (list): Az első jellemzővektor.
+        jellemzok2 (list): A második jellemzővektor.
+
+    Returns:
+        float: Az euklidészi távolság.
+    """
+    tavolsag = 0
+    for i in range(len(vektor1)):
+        tavolsag += (vektor1[i] - vektor2[i])**2
+    return math.sqrt(tavolsag)
 
 # Kaszkád fájl betöltése
 face_cascade_default = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt.xml')
@@ -9,33 +54,23 @@ cap = cv2.VideoCapture(0)
 
 pca = PCA(n_components=1)  # PCA objektum létrehozása
 
+jellemzok = None  # Globális változó - mivel csak egyszer szeretném eltárolni az egyszerűbb összehasonlítás kedvéért
+
+
 while True:
     # Kép beolvasása
     ret, kep = cap.read()
-
-    # Szürkeárnyalatos képpé alakítja
     gray = cv2.cvtColor(kep, cv2.COLOR_BGR2GRAY)
-
-    # Arcok észlelése
     faces_detect = face_cascade_default.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-    try:
-        for (x, y, w, h) in faces_detect:
-            face_extract = gray[y:y + h, x:x + w]  # Arc kivágása
-            arckep_meret = (100, 100)  # Arckép mérete
-            arckep = cv2.resize(face_extract, arckep_meret)  # Arckép átméretezése
-            arckep_vektor = arckep.flatten()  # Kép vektorizálása
-            pca.fit(arckep_vektor.reshape(-1, 1))  # PCA illesztése az arckép vektorra
-            transzformalt_arckep = pca.transform(arckep_vektor.reshape(-1, 1))  # Transzformált arckép
-            jellemzok = transzformalt_arckep.flatten()  # Jellemzők kinyerése
+    if jellemzok is None:
+        jellemzok = keresett_arc_jellemzok(kep) #jellemzok kinyerése csak az első képen
 
-            print(f"Arckép jellemzők: {jellemzok}")
+    for (x, y, w, h) in faces_detect: 
+        cv2.rectangle(kep, (x, y), (x + w, y + h), (0, 0, 0), 2)
 
-            # Arcok rajzolása téglalapban (kék)
-            cv2.rectangle(kep, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    print(f"Arckép jellemzők: {jellemzok}")
 
-    except IndexError:
-        print("Nincs arc a képen.")
 
     # Kép megjelenítése
     cv2.imshow("Arcfelismerő valós időben", kep)
@@ -44,5 +79,13 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Minden adatbázisbeli képhez kiszámítja az euklidészi távolságot
+for adatbazis_jellemzok in adatbazis_jellemzok_lista:
+    tavolsag = euklideszi_tavolsag(jellemzok, adatbazis_jellemzok)
+    print(f"Euklidészi távolság: {tavolsag}")
+
+
 cap.release()
 cv2.destroyAllWindows()
+
+
